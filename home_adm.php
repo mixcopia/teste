807318f399a102baf600d6681067aee520e642e5
Mix Cópia <mixcopiagraf@gmail.com>
2016-04-23 00:37:20 -0300

    arquivos php
    arquivos php

---
+++ home_adm.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<META charset="UTF-8"/ >
+	<link rel="stylesheet" type="text/css" href="css/css.css"/>
+	<link rel="shortcut icon" href="img/favicon.ico">
+
+	<META name="ifal" content="Site finculado ao Instituto Federal de Alagoas"/>
+	<META name="keywords" content="Eventos em Maceió"/>
+	<META name="author" content="estudantes SI 3° Período"/>
+</head>
+<body>
+	<div id="superior">
+		<center><img src="img/ifal.png"; ></center>
+	</div>
+	<div id="wrap">
+
+		<header>
+			<h3>Seja bem Vindo Administrador</h3>
+			<nav>
+				<ul>
+					<li><a href="#">Cadastro de Coordenador de Curso</a></li>
+					<li><a href="#">Cadastro de Coordenador de Evento</a></li>
+					<li><a href="index.html">Sair do Sistema</a></li>
+				</ul>
+			</nav>
+		</header>
+		<div id="content">
+			<article>
+			<?php
+
+			?>
+			</article>
+		</div>
+		<div id="sidebar">
+			<aside>
+				<ul>
+
+				<center>Eventos
+				<a href="http://cssday.io" target="_blank">
+				 <img src="http://www.divyweb.com/site/uploads/2015/12/cssdayio-logo-620x269.png"
+				width="130" align="_center" title="click para saber mais">
+				</a>
+
+
+				<a href="http://javaman.io" target="_blank">
+				<img src="http://harrisgray.files.wordpress.com/2013/09/java-man-tour-banner1.png"
+				width="130" align="_center" title="click para saber mais">
+				</a>
+
+				<a href="http://developer.io" target="_blank">
+				<img src="http://www.mattosdesign.com/wallpapers/imagens/wallpaper_developer_blue.jpg"
+				width="130" align="_center" title="click para saber mais">
+				</a></center>
+
+				</ul>
+			</aside>
+		</div>
+		<br><br><br><br><br><br>
+		<footer>
+
+			<a href="#">Fale Conosco</a>
+		</footer>
+	</div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<META charset="UTF-8"/ >
+	<link rel="stylesheet" type="text/css" href="css/css.css"/>
+	<link rel="shortcut icon" href="img/favicon.ico">
+
+	<META name="ifal" content="Site finculado ao Instituto Federal de Alagoas"/>
+	<META name="keywords" content="Eventos em Maceió"/>
+	<META name="author" content="estudantes SI 3° Período"/>
+</head>
+<body>
+	<div id="superior">
+		<center><img src="img/ifal.png"; ></center>
+	</div>
+		<div id="wrap">
+
+		<div id="content">
+			<center>
+			<h1>Entrar no sistema</h1>
+
+			<form method="post" action="cadastrando.php">
+
+			<input required="required" type="text" name="login" placeholder="login"><br/>
+			<input required="required" type="password" name="password" placeholder="senha"><br/>
+
+			<center><button class="button button1" type="button">Entrar</button></center>
+
+                        <p>Você é aluno e não tem Cadastro?</p>
+                        <a href="#" >
+                        <button class="button button1" type="button">Cadastre-se agora</button> </a><br>
+                        </form></center>
+
+                        <?php
+
+                        ?>
+                        </div>
+
+		<div id="sidebar">
+			<aside>
+
+				<center>Eventos
+				<a href="http://cssday.io" target="_blank">
+				 <img src="http://www.divyweb.com/site/uploads/2015/12/cssdayio-logo-620x269.png"
+				width="130" align="_center" title="click para saber mais">
+				</a>
+
+				<a href="http://javaman.io" target="_blank">
+				<img src="http://harrisgray.files.wordpress.com/2013/09/java-man-tour-banner1.png"
+				width="130" align="_center" title="click para saber mais">
+				</a>
+
+				<a href="http://developer.io" target="_blank">
+				<img src="http://www.mattosdesign.com/wallpapers/imagens/wallpaper_developer_blue.jpg"
+				width="130" align="_center" title="click para saber mais">
+				</a></center>
+
+			</aside>
+		</div>
+		<footer>
+		<br><br><br><br>
+			<a href="#">Fale Conosco</a>
+		</footer>
+	</div>
+</body>
+</html>
